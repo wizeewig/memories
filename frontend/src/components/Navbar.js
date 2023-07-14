b2ac1a2dccb7e2195e79609a05f0fcbf6cfc4837
Ikchhit Kumar Pandey <ikchhit10@gmail.com>
2023-07-14 17:19:16 +0530

    responsive navbar

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -33,11 +33,39 @@ export default function Navbar({login}) {
     }
   }
 
+  const loginstatusMobile=()=>{
+    const token=localStorage.getItem("jwt");
+    if(login || token){
+      return[
+        <>
+        <Link to="/"><li><span class="material-symbols-outlined">home</span></li></Link>
+         <Link to="/profile"><li><span class="material-symbols-outlined">account_circle</span></li></Link>
+         <Link to="/createPost"><li><span class="material-symbols-outlined">add_box</span></li></Link>
+         <Link to="/myfollowingpost" style={{ marginLeft: "20px" }}> <li><span class="material-symbols-outlined">explore</span></li></Link>
+         <Link to="">
+          <li  onClick={()=> setModalOpen(true)}><span class="material-symbols-outlined">logout</span></li>
+         </Link>
+        </>
+      ]
+    }
+    else{
+      return[
+        <>
+          <Link to="/signup"><li>Sign Up</li></Link>
+          <Link to="/signin"><li>Sign In</li></Link>
+        </>
+      ]
+    }
+  }
+
   return (
     <div className='navbar'>
-        <img src={logo1} alt="logo" onClick={()=>{navigate("/")}} ></img>
+        <img id="instaLogo" src={logo1} alt="logo" onClick={()=>{navigate("/")}} ></img>
         <ul className='nav-menu'>
            {loginstatus()}
+        </ul>
+        <ul className='nav-mobile'>
+           {loginstatusMobile()}
         </ul>
         
     </div>
